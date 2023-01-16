c28ee3b6106c5552479842fdb7765c20864641dc
Krzysztof Pecka <pr0s3q@gmail.com>
2023-01-16 17:24:40 +0100

    Change whitespaces

--- DartGUI/Platforms/iOS/AppDelegate.cs
+++ DartGUI/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace DartGUI;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+   protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
