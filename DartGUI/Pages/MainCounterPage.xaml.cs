39dbf08a9e3b168293faa4b918b9ee4b6c9e9c08
pr0s3q <pr0s3q@gmail.com>
2025-05-04 18:58:10 +0200

    Rename objects, to remove unnecessary brackets

--- DartGUI/Pages/MainCounterPage.xaml.cs
+++ DartGUI/Pages/MainCounterPage.xaml.cs
@@ -75,70 +75,68 @@ public partial class MainCounterPage
 
     private void InitializeLayout()
     {
+        var topHsl = new HorizontalStackLayout
         {
-            var hsl = new HorizontalStackLayout
-            {
-                HorizontalOptions = LayoutOptions.Center
-            };
-            var grid = new Grid
-            {
-                ColumnDefinitions = new ColumnDefinitionCollection(
-                    new ColumnDefinition(new GridLength(130.0 * WidthScale)),
-                    new ColumnDefinition(new GridLength(840.0 * WidthScale)),
-                    new ColumnDefinition(new GridLength(130.0 * WidthScale))),
-                RowDefinitions = new RowDefinitionCollection(
-                    new RowDefinition(new GridLength(50.0 * HeightScale))),
-                VerticalOptions = LayoutOptions.Center
-            };
-            var saveGameButton = new Button
-            {
-                BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
-                FontSize = 25.0 * SmallerScale,
-                Margin = new Thickness(1.0 * SmallerScale, 0.0, 1.0 * SmallerScale, 0.0),
-                Padding = new Thickness(0.0, 10.0 * HeightScale, 0.0, 10.0 * HeightScale),
-                WidthRequest = 120.0 * WidthScale,
-                HeightRequest = 50.0 * HeightScale,
-                Text = "Save",
-                TextColor = DesignColors.BUTTON_TEXT_COLOR
-            };
-            saveGameButton.MinimumHeightRequest *= HeightScale;
-            saveGameButton.MinimumWidthRequest *= WidthScale;
-            saveGameButton.Clicked += OnSaveGameButton_Clicked;
-            saveGameButton.Pressed += GlobalEvents.OnButton_Pressed;
-            saveGameButton.Released += GlobalEvents.OnButton_Released;
-            grid.Add(saveGameButton, 0);
+            HorizontalOptions = LayoutOptions.Center
+        };
+        var topGrid = new Grid
+        {
+            ColumnDefinitions = new ColumnDefinitionCollection(
+                new ColumnDefinition(new GridLength(130.0 * WidthScale)),
+                new ColumnDefinition(new GridLength(840.0 * WidthScale)),
+                new ColumnDefinition(new GridLength(130.0 * WidthScale))),
+            RowDefinitions = new RowDefinitionCollection(
+                new RowDefinition(new GridLength(50.0 * HeightScale))),
+            VerticalOptions = LayoutOptions.Center
+        };
+        var saveGameButton = new Button
+        {
+            BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
+            FontSize = 25.0 * SmallerScale,
+            Margin = new Thickness(1.0 * SmallerScale, 0.0, 1.0 * SmallerScale, 0.0),
+            Padding = new Thickness(0.0, 10.0 * HeightScale, 0.0, 10.0 * HeightScale),
+            WidthRequest = 120.0 * WidthScale,
+            HeightRequest = 50.0 * HeightScale,
+            Text = "Save",
+            TextColor = DesignColors.BUTTON_TEXT_COLOR
+        };
+        saveGameButton.MinimumHeightRequest *= HeightScale;
+        saveGameButton.MinimumWidthRequest *= WidthScale;
+        saveGameButton.Clicked += OnSaveGameButton_Clicked;
+        saveGameButton.Pressed += GlobalEvents.OnButton_Pressed;
+        saveGameButton.Released += GlobalEvents.OnButton_Released;
+        topGrid.Add(saveGameButton, 0);
 
-            var lastThreeShotsLabel = new Label
-            {
-                FontSize = 30.0 * SmallerScale,
-                HorizontalTextAlignment = TextAlignment.Center,
-                Text = string.Empty,
-                TextColor = DesignColors.LABEL_TEXT_COLOR
-            };
-            grid.Add(lastThreeShotsLabel, 1);
+        var lastThreeShotsLabel = new Label
+        {
+            FontSize = 30.0 * SmallerScale,
+            HorizontalTextAlignment = TextAlignment.Center,
+            Text = string.Empty,
+            TextColor = DesignColors.LABEL_TEXT_COLOR
+        };
+        topGrid.Add(lastThreeShotsLabel, 1);
 
-            _game.AddLastThreeShotsLabel(lastThreeShotsLabel);
+        _game.AddLastThreeShotsLabel(lastThreeShotsLabel);
 
-            var statisticsButton = new Button
-            {
-                BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
-                FontSize = 25.0 * SmallerScale,
-                Margin = new Thickness(1.0 * SmallerScale, 0.0, 1.0 * SmallerScale, 0.0),
-                Padding = new Thickness(0.0, 10.0 * HeightScale, 0.0, 10.0 * HeightScale),
-                WidthRequest = 120.0 * WidthScale,
-                HeightRequest = 50.0 * HeightScale,
-                Text = "Statistics",
-                TextColor = DesignColors.BUTTON_TEXT_COLOR
-            };
-            statisticsButton.MinimumHeightRequest *= HeightScale;
-            statisticsButton.MinimumWidthRequest *= WidthScale;
-            statisticsButton.Clicked += OnStatisticsButton_Clicked;
-            statisticsButton.Pressed += GlobalEvents.OnButton_Pressed;
-            statisticsButton.Released += GlobalEvents.OnButton_Released;
-            grid.Add(statisticsButton, 2);
-            hsl.Add(grid);
-            VerticalStackLayout.Add(hsl);
-        }
+        var statisticsButton = new Button
+        {
+            BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
+            FontSize = 25.0 * SmallerScale,
+            Margin = new Thickness(1.0 * SmallerScale, 0.0, 1.0 * SmallerScale, 0.0),
+            Padding = new Thickness(0.0, 10.0 * HeightScale, 0.0, 10.0 * HeightScale),
+            WidthRequest = 120.0 * WidthScale,
+            HeightRequest = 50.0 * HeightScale,
+            Text = "Statistics",
+            TextColor = DesignColors.BUTTON_TEXT_COLOR
+        };
+        statisticsButton.MinimumHeightRequest *= HeightScale;
+        statisticsButton.MinimumWidthRequest *= WidthScale;
+        statisticsButton.Clicked += OnStatisticsButton_Clicked;
+        statisticsButton.Pressed += GlobalEvents.OnButton_Pressed;
+        statisticsButton.Released += GlobalEvents.OnButton_Released;
+        topGrid.Add(statisticsButton, 2);
+        topHsl.Add(topGrid);
+        VerticalStackLayout.Add(topHsl);
 
         for (var i = 0; i < 3; i++)
         {
@@ -170,45 +168,43 @@ public partial class MainCounterPage
             VerticalStackLayout.Add(hsl);
         }
 
+        var middleHsl = new HorizontalStackLayout
         {
-            var hsl = new HorizontalStackLayout
-            {
-                HorizontalOptions = LayoutOptions.Center
-            };
-            var button = new Button
-            {
-                BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
-                FontSize = 25.0 * SmallerScale,
-                Margin = new Thickness(5.0 * WidthScale, 0.0, 5.0 * WidthScale, 0.0),
-                Padding = new Thickness(0.0, 5.0 * HeightScale, 0.0, 5.0 * HeightScale),
-                WidthRequest = 120.0 * WidthScale,
-                Text = "Undo",
-                TextColor = DesignColors.BUTTON_TEXT_COLOR
-            };
-            button.MinimumHeightRequest *= HeightScale;
-            button.MinimumWidthRequest *= WidthScale;
-            button.Clicked += OnUndoButton_Clicked;
-            button.Pressed += GlobalEvents.OnButton_Pressed;
-            button.Released += GlobalEvents.OnButton_Released;
-            hsl.Add(button);
-            button = new Button
-            {
-                BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
-                FontSize = 25.0 * SmallerScale,
-                Margin = new Thickness(5.0 * WidthScale, 0.0, 5.0 * WidthScale, 0.0),
-                Padding = new Thickness(0.0, 5.0 * HeightScale, 0.0, 5.0 * HeightScale),
-                WidthRequest = 120.0 * WidthScale,
-                Text = "Accept",
-                TextColor = DesignColors.BUTTON_TEXT_COLOR
-            };
-            button.MinimumHeightRequest *= HeightScale;
-            button.MinimumWidthRequest *= WidthScale;
-            button.Clicked += OnAcceptButton_Clicked;
-            button.Pressed += GlobalEvents.OnButton_Pressed;
-            button.Released += GlobalEvents.OnButton_Released;
-            hsl.Add(button);
-            VerticalStackLayout.Add(hsl);
-        }
+            HorizontalOptions = LayoutOptions.Center
+        };
+        var undoButton = new Button
+        {
+            BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
+            FontSize = 25.0 * SmallerScale,
+            Margin = new Thickness(5.0 * WidthScale, 0.0, 5.0 * WidthScale, 0.0),
+            Padding = new Thickness(0.0, 5.0 * HeightScale, 0.0, 5.0 * HeightScale),
+            WidthRequest = 120.0 * WidthScale,
+            Text = "Undo",
+            TextColor = DesignColors.BUTTON_TEXT_COLOR
+        };
+        undoButton.MinimumHeightRequest *= HeightScale;
+        undoButton.MinimumWidthRequest *= WidthScale;
+        undoButton.Clicked += OnUndoButton_Clicked;
+        undoButton.Pressed += GlobalEvents.OnButton_Pressed;
+        undoButton.Released += GlobalEvents.OnButton_Released;
+        middleHsl.Add(undoButton);
+        var acceptButton = new Button
+        {
+            BackgroundColor = DesignColors.BUTTON_BACKGROUND_COLOR,
+            FontSize = 25.0 * SmallerScale,
+            Margin = new Thickness(5.0 * WidthScale, 0.0, 5.0 * WidthScale, 0.0),
+            Padding = new Thickness(0.0, 5.0 * HeightScale, 0.0, 5.0 * HeightScale),
+            WidthRequest = 120.0 * WidthScale,
+            Text = "Accept",
+            TextColor = DesignColors.BUTTON_TEXT_COLOR
+        };
+        acceptButton.MinimumHeightRequest *= HeightScale;
+        acceptButton.MinimumWidthRequest *= WidthScale;
+        acceptButton.Clicked += OnAcceptButton_Clicked;
+        acceptButton.Pressed += GlobalEvents.OnButton_Pressed;
+        acceptButton.Released += GlobalEvents.OnButton_Released;
+        middleHsl.Add(acceptButton);
+        VerticalStackLayout.Add(middleHsl);
 
         var currentDataLabel = new Label
         {
@@ -220,102 +216,100 @@ public partial class MainCounterPage
         VerticalStackLayout.Add(currentDataLabel);
         _game.AddCurrentDataLabel(currentDataLabel);
 
+        var bottomHsl = new HorizontalStackLayout
         {
-            var hsl = new HorizontalStackLayout
+            HorizontalOptions = LayoutOptions.Center
+        };
+        var grid = new Grid
+        {
+            ColumnDefinitions = new ColumnDefinitionCollection(
+                new ColumnDefinition(new GridLength(50.0 * WidthScale)),
+                new ColumnDefinition(new GridLength(250.0 * WidthScale)),
+                new ColumnDefinition(new GridLength(75.0 * WidthScale)),
+                new ColumnDefinition(new GridLength(125.0 * WidthScale))),
+            RowDefinitions = new RowDefinitionCollection(
+                new RowDefinition(new GridLength(50.0 * HeightScale)),
+                new RowDefinition(new GridLength(50.0 * HeightScale)),
+                new RowDefinition(new GridLength(50.0 * HeightScale)),
+                new RowDefinition(new GridLength(50.0 * HeightScale)),
+                new RowDefinition(new GridLength(50.0 * HeightScale))),
+            VerticalOptions = LayoutOptions.Center
+        };
+
+        for (var i = 0; i < _playerNames.Count; i++)
+        {
+            // Player ID
+            var playerIdBorder = new Border
             {
-                HorizontalOptions = LayoutOptions.Center
+                Stroke = DesignColors.BORDER_COLOR,
+                StrokeThickness = 2.0 * SmallerScale
             };
-            var grid = new Grid
+            var playerIdLabel = new Label
             {
-                ColumnDefinitions = new ColumnDefinitionCollection(
-                    new ColumnDefinition(new GridLength(50.0 * WidthScale)),
-                    new ColumnDefinition(new GridLength(250.0 * WidthScale)),
-                    new ColumnDefinition(new GridLength(75.0 * WidthScale)),
-                    new ColumnDefinition(new GridLength(125.0 * WidthScale))),
-                RowDefinitions = new RowDefinitionCollection(
-                    new RowDefinition(new GridLength(50.0 * HeightScale)),
-                    new RowDefinition(new GridLength(50.0 * HeightScale)),
-                    new RowDefinition(new GridLength(50.0 * HeightScale)),
-                    new RowDefinition(new GridLength(50.0 * HeightScale)),
-                    new RowDefinition(new GridLength(50.0 * HeightScale))),
-                VerticalOptions = LayoutOptions.Center
+                FontSize = 30.0 * SmallerScale,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = (i + 1).ToString(),
+                TextColor = DesignColors.LABEL_TEXT_COLOR
             };
+            playerIdBorder.Content = playerIdLabel;
+            grid.Add(playerIdBorder, 0, i);
 
-            for (var i = 0; i < _playerNames.Count; i++)
+            // Player Name
+            var playerNameBorder = new Border
             {
-                // Player ID
-                var playerIdBorder = new Border
-                {
-                    Stroke = DesignColors.BORDER_COLOR,
-                    StrokeThickness = 2.0 * SmallerScale
-                };
-                var playerIdLabel = new Label
-                {
-                    FontSize = 30.0 * SmallerScale,
-                    HorizontalTextAlignment = TextAlignment.Center,
-                    Text = (i + 1).ToString(),
-                    TextColor = DesignColors.LABEL_TEXT_COLOR
-                };
-                playerIdBorder.Content = playerIdLabel;
-                grid.Add(playerIdBorder, 0, i);
+                Stroke = DesignColors.BORDER_COLOR,
+                StrokeThickness = 2.0 * SmallerScale
+            };
+            var playerNameLabel = new Label
+            {
+                FontSize = 30.0 * SmallerScale,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = _game.GetPlayerName(i),
+                TextColor = DesignColors.LABEL_TEXT_COLOR
+            };
+            playerNameBorder.Content = playerNameLabel;
+            grid.Add(playerNameBorder, 1, i);
 
-                // Player Name
-                var playerNameBorder = new Border
-                {
-                    Stroke = DesignColors.BORDER_COLOR,
-                    StrokeThickness = 2.0 * SmallerScale
-                };
-                var playerNameLabel = new Label
-                {
-                    FontSize = 30.0 * SmallerScale,
-                    HorizontalTextAlignment = TextAlignment.Center,
-                    Text = _game.GetPlayerName(i),
-                    TextColor = DesignColors.LABEL_TEXT_COLOR
-                };
-                playerNameBorder.Content = playerNameLabel;
-                grid.Add(playerNameBorder, 1, i);
+            // Legs won
+            var legsWonBorder = new Border
+            {
+                Stroke = DesignColors.BORDER_COLOR,
+                StrokeThickness = 2.0 * SmallerScale
+            };
+            var legsWonLabel = new Label
+            {
+                FontSize = 30.0 * SmallerScale,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = _game.GetPlayerLegsWon(i).ToString(),
+                TextColor = DesignColors.LABEL_TEXT_COLOR
+            };
+            legsWonBorder.Content = legsWonLabel;
+            grid.Add(legsWonBorder, 2, i);
 
-                // Legs won
-                var legsWonBorder = new Border
-                {
-                    Stroke = DesignColors.BORDER_COLOR,
-                    StrokeThickness = 2.0 * SmallerScale
-                };
-                var legsWonLabel = new Label
-                {
-                    FontSize = 30.0 * SmallerScale,
-                    HorizontalTextAlignment = TextAlignment.Center,
-                    Text = _game.GetPlayerLegsWon(i).ToString(),
-                    TextColor = DesignColors.LABEL_TEXT_COLOR
-                };
-                legsWonBorder.Content = legsWonLabel;
-                grid.Add(legsWonBorder, 2, i);
+            // Points left
+            var pointsLeftBorder = new Border
+            {
+                Stroke = DesignColors.BORDER_COLOR,
+                StrokeThickness = 2.0 * SmallerScale
+            };
+            var pointsLeftLabel = new Label
+            {
+                FontSize = 30.0 * SmallerScale,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = _game.GetPlayerPointsLeft(i).ToString(),
+                TextColor = DesignColors.LABEL_TEXT_COLOR
+            };
+            pointsLeftBorder.Content = pointsLeftLabel;
+            grid.Add(pointsLeftBorder, 3, i);
 
-                // Points left
-                var pointsLeftBorder = new Border
-                {
-                    Stroke = DesignColors.BORDER_COLOR,
-                    StrokeThickness = 2.0 * SmallerScale
-                };
-                var pointsLeftLabel = new Label
-                {
-                    FontSize = 30.0 * SmallerScale,
-                    HorizontalTextAlignment = TextAlignment.Center,
-                    Text = _game.GetPlayerPointsLeft(i).ToString(),
-                    TextColor = DesignColors.LABEL_TEXT_COLOR
-                };
-                pointsLeftBorder.Content = pointsLeftLabel;
-                grid.Add(pointsLeftBorder, 3, i);
-
-                _game.AddRowData(playerIdBorder, playerIdLabel,
-                    playerNameBorder, playerNameLabel,
-                    legsWonBorder, legsWonLabel,
-                    pointsLeftBorder, pointsLeftLabel);
-            }
-
-            hsl.Add(grid);
-            VerticalStackLayout.Add(hsl);
+            _game.AddRowData(playerIdBorder, playerIdLabel,
+                playerNameBorder, playerNameLabel,
+                legsWonBorder, legsWonLabel,
+                pointsLeftBorder, pointsLeftLabel);
         }
+
+        bottomHsl.Add(grid);
+        VerticalStackLayout.Add(bottomHsl);
 
         VerticalStackLayout.Spacing *= HeightScale;
     }
